5c2ef4d6e9c102a7db5db5ed37c537968bcb1c9c
Ankit <ankit205123@gmail.com>
2020-12-18 16:07:02 +0530

    neww repo

---
+++ DemoApp/src/com/lti/model/Area.java
@@ -0,0 +1,7 @@
+package com.lti.model;
+
+public class Area {
+	public void areaOfTriangle(int b,int h) {
+		System.out.println("area of triangle is "+(b*h/2));
+	}
+}
---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,10 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("hello world");
+	}
+	public void method() {
+		System.out.println("hii all, how are you doing");
+	}
+}
