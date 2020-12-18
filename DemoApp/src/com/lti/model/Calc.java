b1566aa95ea60514c518875f2a9503e375fd10d3
Ankit <ankit205123@gmail.com>
2020-12-18 16:28:19 +0530

    modified

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -2,7 +2,7 @@ package com.lti.model;
 
 public class Calc {
 	public void add(int num1, int num2) {
-		System.out.println("sum= " + (num1 + num2));
+		System.out.println("sumation= " + ((num1 + num2)));
 	}
 	
 }
